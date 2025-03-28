ca1bc585a0267379bbc6196ae4e62387226530e6
sebastian krantz <sebastiankrantz@sebastians-MacBook-Pro.local>
2025-03-28 11:43:31 +0100

    Get distances data.

--- code/distances.R
+++ code/distances.R
@@ -17,7 +17,7 @@ NUTS1 <- NUTS |> subset(levl_code == 1) |> st_transform(4326)
 #### Define NASA bearer token
 bearer <- "eyJ0eXAiOiJKV1QiLCJvcmlnaW4iOiJFYXJ0aGRhdGEgTG9naW4iLCJzaWciOiJlZGxqd3RwdWJrZXlfb3BzIiwiYWxnIjoiUlMyNTYifQ.eyJ0eXBlIjoiVXNlciIsInVpZCI6InNlYmtyYW50eiIsImV4cCI6MTc0ODMwNzE5NCwiaWF0IjoxNzQzMTIzMTk0LCJpc3MiOiJodHRwczovL3Vycy5lYXJ0aGRhdGEubmFzYS5nb3YiLCJpZGVudGl0eV9wcm92aWRlciI6ImVkbF9vcHMiLCJhY3IiOiJlZGwiLCJhc3N1cmFuY2VfbGV2ZWwiOjN9.CTnC0Do3llf-wt_hba-OkGJBhXzKRshWBFpK4cx3xtJaLauLS7SiAeUrMR1WqAbq9h4R30E8PUCVnnHRMZd5uf5D6VTXxhhqEVT-4yUt33Ji1Q6pzHUwIb30uqKMkggxb_n4gL-8s3oot4b6PwutiU4bgsZX_YALakTeLHnLg8Ry9r9zVFmOJeorYgCpWRsx0XdqT-_YNmYA8MrK9RNZ3FmFh2Hs3_xN_KPBgMGEb6jpNciBQiir1cjvFQuBPWGP-Eix1PfmBN-DJuKXdp35tKGduJtUTpKxJ-n_ZJLCgJZ5Jw63UewHJduu0kziPy3IYBO8ufMkufv3vv_uSyvK7A"
 NL21 <- vector("list", length = nrow(NUTS1))
-for (i in seq_along(NL21)) {
+for (i in seq_along(NL21)[-(1:124)]) {
   print(NUTS1$nuts_name[i])
   NL21[[i]] <- bm_raster(roi_sf = NUTS1[i,1],
                          product_id = "VNP46A4",
@@ -25,3 +25,7 @@ for (i in seq_along(NL21)) {
                          bearer = bearer) |>
                as.data.frame(xy = TRUE)
 }
+names(NL21) <- NUTS1$nuts_id
+qs::qsave(NL21, "data/NL21.qs")
+
+NL21 <- rowbind(NL21, idcol = "NUTS1")
