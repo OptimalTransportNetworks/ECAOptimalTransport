63a1409c9fd5752719733b100aba91a8e6e5c46a
SebKrantz <sebastian.krantz@graduateinstitute.ch>
2025-05-02 15:04:58 +0200

    Improvements: using full cells.

--- code/get_network_fajgelbaum_graff.R
+++ code/get_network_fajgelbaum_graff.R
@@ -1,13 +1,10 @@
 library(fastverse)
 set_collapse(mask = c("manip", "helper", "special"), nthreads = 4)
-fastverse_extend(qs, sf, s2, units, stplanr, sfnetworks, osrm, tmap, install = TRUE)
+fastverse_extend(qs, sf, units, stplanr, osrm, install = TRUE)
 source("code/helpers.R")
 fastverse_conflicts()
 
-####################################
-# Part 1: Important Cities and Ports
-####################################
-
+# Important Local GDP Data ----------------------------
 countries <- wbstats::wb_cachelist$countries %$% iso3c[region_iso3c %==% "ECS"] %>% 
   c("XKO") %>% setdiff(c("GRL", "ISL", "GBR", "IRL", "NOR", "SWE", "FIN", "FRO", "IMN", "CYP"))
 
@@ -17,100 +14,145 @@ ECA_outline <- st_read("/Users/sebastiankrantz/Documents/ECAOptimalTransport/dat
 
 # Local GDP data: 0.5 degree, censored for density below 0.01
 LGDP <- fread("/Users/sebastiankrantz/Documents/Data/LocalGDP/0_5deg/final_GDP_0_5deg_postadjust_pop_dens_0_01_adjust.csv") |> 
-        subset(iso %in% countries) |>
+        subset(iso %in% countries) |> 
+        slice(cell_id, iso, subcell_id, how = "max", order.by = year) |> 
         transform(longitude = longitude + 0.25, # Coordinate is bottom left corner
-                  latitude = latitude + 0.25) 
+                  latitude = latitude + 0.25) %>% 
+        collap(~ cell_id + subcell_id, w = ~ pop_cell+1e-5, 
+               custom = list(fsum_uw = gvr(., "_GCP_", return = "names"), 
+                             fmean = gvr(., "_GDPC_|itude|censored|year|national", return = "names"), 
+                             fmode = is_categorical))
   
 LGDP %<>% subset(LGDP |> st_as_sf(coords = c("longitude", "latitude"), crs = 4326) |> 
                  st_intersects(ECA_outline, sparse = FALSE))
-shape <- st_read("/Users/sebastiankrantz/Documents/Data/LocalGDP/0_5deg/shapefile") |>
-         base::subset(cell_id %in% LGDP$cell_id)
-shape %<>% join(LGDP, on = c("cell_id", "subcell_id"), how = "inner", drop = TRUE)
-shape %<>% st_make_valid() 
+# shape <- st_read("/Users/sebastiankrantz/Documents/Data/LocalGDP/0_5deg/shapefile") |>
+#          base::subset(cell_id %in% LGDP$cell_id)
+# shape %<>% join(LGDP, on = c("cell_id", "iso", "subcell_id"), how = "inner", drop = TRUE)
+# shape %<>% st_make_valid() 
 
-mapview::mapview(shape, zcol = "pop_cell") + 
+# Constructing clean shapes
+shape <- Map(
+  f = function(x, y) {
+    st_polygon(list(cbind(x = c(x-0.25, x+0.25, x+0.25, x-0.25, x-0.25), 
+                          y = c(y-0.25, y-0.25, y+0.25, y+0.25, y-0.25))))
+  },
+  x = LGDP$longitude,
+  y = LGDP$latitude
+) |> st_as_sfc(crs = 4326) |> 
+  st_sf(LGDP, geometry = _, crs = 4326)
+
+if(FALSE) mapview::mapview(shape, zcol = "pop_cell") + 
+  mapview::mapview(LGDP |> st_as_sf(coords = c("longitude", "latitude"), crs = 4326), zcol = "pop_cell") +
   mapview::mapview(ECA_outline, color = "red")
 
 # Creating Adjacency grid  -----------------------------------------------
 
 # queen = TRUE  →  edge or corner touching
-neigh <- spdep::poly2nb(shape, queen = TRUE)
-neigh <- data.frame(from = rep(seq_len(nrow(shape)), lengths(neigh)),
-                    to   = unlist(neigh, use.names = FALSE)) 
+# gap_fix_snap(shape, tol = deg_m(5000)) |> mapview::mapview()
+# neigh <- shape |> spdep::poly2nb(queen = TRUE, snap = deg_m(1000))
+# table(spdep::card(neigh))
+# neigh <- data.frame(from = rep(seq_along(neigh), spdep::card(neigh)),
+#                     to   = unlist(unclass(neigh)[spdep::card(neigh) > 0], use.names = FALSE)) 
+
+# Manual: better!
+settfmv(shape, c("longitude", "latitude"), round, 2)
+neigh <- shape |> with(cbind(longitude, latitude)) |> mrtl() |> 
+  lapply(function(r) {
+    x <- r[1L]; y = r[2L]
+    fmatch(list(lon = c(x-0.5, x-0.5, x-0.5, x, x+0.5, x+0.5, x+0.5, x), 
+                lat = c(y-0.5, y, y+0.5, y+0.5, y+0.5, y, y-0.5, y-0.5)), 
+           .subset(shape, c("longitude", "latitude"))) |> na_rm()
+})
+
+table(lengths(neigh))
+neigh <- data.frame(from = rep(seq_along(neigh), lengths(neigh)),
+                    to   = unlist(neigh[lengths(neigh) > 0], use.names = FALSE)) 
+
 # Remove duplicates
 neigh %<>% mutate(from_to = pmin(from, to), 
                   to_from = pmax(to, from)) %>%
   unique(cols = c("from_to", "to_from")) %>% 
-  roworder(from, to) %>%
-  subset(from != to & from > 0 & to > 0 & rowid(from) < 9) %>% 
-  roworder(to, from) %>%
-  subset(rowid(to) < 9)
+  subset(from != to & from > 0 & to > 0)
   
 # Now we can create the adjacency matrix
-mapview::mapview(subset(shape, unique(c(neigh$from, neigh$to)))) +
+if(FALSE) mapview::mapview(subset(shape, unique(c(neigh$from, neigh$to)))) +
   mapview::mapview(st_as_sf(qDF(atomic_elem(shape)), coords = c("longitude", "latitude"), crs = 4326))
 
-if(FALSE) {
-# Getting population cetroids
+if(TRUE) { # Population weighting
+# Getting population centroids
 WPOP <- terra::rast("/Users/sebastiankrantz/Documents/Data/WorldPop/ppp_2020_1km_Aggregated.tif")
   
 # Segments
-ext <- terra::extract(WPOP, shape,
-                      cells   = TRUE,   # return the raster cell number
-                      weights = TRUE,   # return fractional coverage
-                      na.rm   = TRUE)   # drop cells where population is NA
-
-# Append the x / y coordinates of every raster cell
-xy <- terra::xyFromCell(WPOP, ext$cell)
-ext$x <- xy[, 1]
-ext$y <- xy[, 2]
+ext <- exactextractr::exact_extract(WPOP, shape, include_xy = TRUE) |> rowbind(idcol = "ID")
 
 # Population-weighted mean of x and y within each polygon
-setDT(ext)                                            # data.table magic
-ext[, pop_weight := ppp_2020_1km_Aggregated * weight] # weight = pop * area share
-
-ext %<>% group_by(ID) %>% 
+ext %<>% 
+  mutate(pop_weight = value * coverage_fraction) %>%
+  group_by(ID) %>% 
   summarise(across(c(x, y), fmean, pop_weight),
-            pop_weight = fsum(pop_weight)) %>% 
-  mutate(ID = as.integer(ID))
+            pop_weight = fsum(pop_weight)) 
 
-add_vars(shape) <- ext |> roworder(ID) |> select(pwx = x, pwy = y, pop_weight) 
+if(FALSE) {  
+# Centroids not in the shape
+not_inside <- which(!s2::s2_within(with(ext, s2::s2_lnglat(x, y)), shape$geometry))
+
+# Getting nearest point
+nearest_points <- st_nearest_points(shape[not_inside, ],
+  ext[not_inside, ] |> st_as_sf(coords = c("x", "y"), crs = 4326), pairwise = TRUE) |> 
+  st_cast("POINT", group_or_split = FALSE, warn = FALSE) |> st_as_sf()
+
+if(FALSE) ext[not_inside, ] |>
+  st_as_sf(coords = c("x", "y"), crs = 4326) |>
+  mapview::mapview(zcol = "pop_weight", layer.name = "Population") +
+  mapview::mapview(shape[not_inside, ]) +
+  mapview::mapview(nearest_points)
+
+# Replacing 
+set(ext, not_inside, c("x", "y"), mctl(st_coordinates(nearest_points)))
+}
+
+tfm(shape) <- ext |> roworder(ID) |> select(pwx = x, pwy = y, pop_weight) 
 settfm(shape, pwx = pfirst(pwx, longitude), pwy = pfirst(pwy, latitude))
 
 # Test
-mapview::mapview(subset(shape, unique(c(neigh$from, neigh$to)))) +
+if(FALSE) mapview::mapview(subset(shape, unique(c(neigh$from, neigh$to)))) +
   mapview::mapview(st_as_sf(qDF(atomic_elem(shape)), coords = c("pwx", "pwy"), crs = 4326))
 } # -> segments with population weighted data doesn't work too well
 
+# Saving shape
+qsave(shape, "data/grid_network/cells_shape.qs")
 
 # Now generating segments
-lon_col <- "longitude"
-lat_col <- "latitude"
+lon_col <- shape[["pwx"]] # "longitude"
+lat_col <- shape[["pwy"]] # "latitude"
 
 segments <- Map(
   f = function(i, j) {
-    st_linestring(matrix(c(shape[[lon_col]][i], shape[[lon_col]][j], 
-                           shape[[lat_col]][i], shape[[lat_col]][j]), 2))
+    st_linestring(matrix(c(lon_col[i], lon_col[j], 
+                           lat_col[i], lat_col[j]), 2))
   },
   neigh$from,
   neigh$to
 )
 
-segments <- st_sfc(segments, crs = 4326)
-neigh_lines <- st_sf(neigh, geometry = segments)
+segments <- st_sf(neigh, geometry = st_sfc(segments, crs = 4326))
 
-mapview::mapview(neigh_lines) 
+if(FALSE) mapview::mapview(segments) 
+# line2df(segments)
+# remove <- c(8461, 8462, 8454, 8457, 8455, 8449, 8453, 8442, 8443, 
+#             8435, )
 
-line2df(neigh_lines)
+# Saving segments
+qsave(segments, "data/grid_network/segments.qs")
+
 
 # Now scraping the routing data following Graff (2024)
-library(osrm)
-routes <- data.table(from = neigh_lines$from, 
-                     to = neigh_lines$to, 
-                     from_lon = shape$longitude[neigh_lines$from],
-                     from_lat = shape$latitude[neigh_lines$from],
-                     to_lon = shape$longitude[neigh_lines$to],
-                     to_lat = shape$latitude[neigh_lines$to],
+routes <- data.table(from = segments$from, 
+                     to = segments$to, 
+                     from_lon = lon_col[segments$from],
+                     from_lat = lat_col[segments$from],
+                     to_lon = lon_col[segments$to],
+                     to_lat = lat_col[segments$to],
                      duration = NA_real_, 
                      distance = NA_real_, 
                      geometry = list())
@@ -156,6 +198,7 @@ routes %$% descr(start_duration_10kmh / duration)
 routes %$% descr(end_duration_10kmh / duration)
 
 settfm(routes, 
+  speed_kmh = distance / ((duration + start_duration_10kmh + end_duration_10kmh) / 60),
   route_efficiency = sp_distance / (distance + start_distance + end_distance),
   time_efficiency = sp_distance / ((duration + start_duration_10kmh + end_duration_10kmh) / 60)
 )
@@ -164,6 +207,12 @@ descr(routes$route_efficiency)
 descr(routes$time_efficiency)
 
 # Saving
-routes |> qsave("data/grid_network/routes_raw.qs")
+routes |> qsave("data/grid_network/routes_raw_pw.qs")
 
-
+# Plotting
+mapview::mapview(shape) + (segments |> 
+  join(routes, on = c("from", "to"), how = "inner", drop = TRUE) |> 
+  select(time_efficiency) |> 
+  mapview::mapview())
+  
+  
